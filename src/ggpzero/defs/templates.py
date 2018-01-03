da01bf35513a848b0e4618d3f900c712f0e72eb0
Richard Emslie <richardemslie@gmail.com>
2018-01-03 18:08:00 +0000

    code review by myself, posting to online chat and noticing a bunch of issues

--- src/ggpzero/defs/templates.py
+++ src/ggpzero/defs/templates.py
@@ -15,11 +15,10 @@ def nn_model_config_template(game, network_size_hint="small"):
     conf.input_columns = transformer.num_cols
     conf.input_channels = transformer.num_channels
 
-    conf.input_others = transformer.number_of_non_cord_states
     conf.policy_dist_count = transformer.policy_dist_count
 
     # ensure no regularisation
-    conf.alphazero_regularisation = False
+    conf.l2_regularisation = False
 
     # use defaults
     conf.learning_rate = None
@@ -29,42 +28,39 @@ def nn_model_config_template(game, network_size_hint="small"):
     conf.dropout_rate_policy = 0.333
     conf.dropout_rate_value = 0.5
 
+    # ideally residual_layers is the same size as board size
+
     if network_size_hint == "tiny":
         conf.residual_layers = 4
         conf.cnn_filter_size = 32
         conf.value_hidden_size = 32
-        conf.max_hidden_other_size = 32
 
     elif network_size_hint == "smaller":
-        conf.residual_layers = 4
+        conf.residual_layers = 8
         conf.cnn_filter_size = 64
         conf.value_hidden_size = 64
-        conf.max_hidden_other_size = 64
 
     elif network_size_hint == "small":
-        conf.residual_layers = 6
+        conf.residual_layers = 8
         conf.cnn_filter_size = 96
         conf.value_hidden_size = 128
-        conf.max_hidden_other_size = 128
 
     elif network_size_hint == "normal":
         conf.residual_layers = 8
         conf.cnn_filter_size = 112
-        conf.value_hidden_size = 129
-        conf.max_hidden_other_size = 128
-
-    elif network_size_hint == "larger":
-        conf.residual_layers = 8
-        conf.cnn_filter_size = 128
-        conf.value_hidden_size = 256
-        conf.max_hidden_other_size = 128
+        conf.value_hidden_size = 128
 
     elif network_size_hint == "large":
         conf.residual_layers = 8
+        conf.cnn_filter_size = 128
+        conf.value_hidden_size = 256
+
+    elif network_size_hint == "larger":
+        conf.residual_layers = 8
         conf.cnn_filter_size = 192
         conf.value_hidden_size = 256
-        conf.max_hidden_other_size = 128
     else:
         assert False, "network_size_hint %s, not recognised" % network_size_hint
 
     return conf
+
