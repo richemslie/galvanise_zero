90a73bc73e3177c0ed9dc68c707afcc30e204e6f
Richard Emslie <richardemslie@gmail.com>
2018-06-01 18:04:54 +0100

    refactor model code, and add my own model concoction (which is admittedly a bit bonkers), based on bn/act/conv all the way - and it seems much (much) more resilient to overfitting than existing resnet

--- src/ggpzero/nn/model.py
+++ src/ggpzero/nn/model.py
@@ -4,88 +4,141 @@ from ggpzero.util.keras import is_channels_first, keras_models, keras_regularize
 from ggpzero.util.keras import keras_layers as klayers
 
 
-# kind of thought keras was taking care of this XXX
 def get_bn_axis():
     return 1 if is_channels_first() else 3
 
 
-def Conv2DBlock(*args, **kwds):
+def act(x, activation, name):
+    if activation == "leakyrelu":
+        x = klayers.LeakyReLU(alpha=0.03, name=name)(x)
+    else:
+        x = klayers.Activation(activation, name=name)(x)
+    return x
+
+
+def bn(x, name):
+    return klayers.BatchNormalization(axis=get_bn_axis(),
+                                      name=name)(x)
+
+
+def conv2d_block(*args, **kwds):
     # args, kwds -> passed through to Conv2D
 
-    assert "activation" in kwds
     activation = kwds.pop("activation")
-
-    try:
-        name = kwds.pop('name',)
-    except KeyError:
-        name = 'conv2d_block'
-
-    conv_name = name + '_conv2d'
-    bn_name = name + '_bn'
-    act_name = name + '_act'
-
-    do_bn = activation != "selu"
+    name = kwds.pop('name')
 
     def block(x):
         x = klayers.Conv2D(*args,
-                           name=conv_name, **kwds)(x)
+                           name=name + '_conv2d',
+                           **kwds)(x)
 
-        if do_bn:
-            x = klayers.BatchNormalization(axis=get_bn_axis(),
-                                           name=bn_name)(x)
-
-        if activation == "leakyrelu":
-            x = klayers.LeakyReLU(alpha=0.03, name=act_name)(x)
-        else:
-            x = klayers.Activation(activation, name=act_name)(x)
-        return x
+        x = bn(x, name + '_bn')
+        return act(x, activation, name + '_act')
 
     return block
 
 
-def ResidualBlock(*args, **kwds):
-    # args, kwds -> passed through to Conv2D
-    assert "padding" not in kwds
+def residual_block_v1(cnn_filter_size, cnn_kernel_size,
+                      simple=False, dropout=-1, prefix="residual_block_",
+                      **kwds):
 
-    name = kwds.pop('name', 'residual_block')
-
-    name_conv0 = name + "_conv0"
-    name_conv1 = name + "_conv1"
-    name_bn0 = name + "_bn0"
-    name_bn1 = name + "_bn1"
-    name_act0 = name + "_act0"
-    name_act_after = name + "_act_after"
-    name_add = name + "_add"
+    # kwds -> passed through to Conv2D
+    for n in "padding use_bias name":
+        assert n not in kwds
 
     activation = kwds.pop('activation', "relu")
 
-    def block(tensor):
-        x = klayers.Conv2D(*args,
-                           name=name_conv0,
+    def conv(x, name, kernel_size=None):
+        name = prefix + name
+        if kernel_size is None:
+            kernel_size = cnn_kernel_size
+        x = klayers.Conv2D(cnn_filter_size, kernel_size,
+                           name=name,
                            padding="same",
-                           **kwds)(tensor)
-
-        x = klayers.BatchNormalization(axis=get_bn_axis(),
-                                       name=name_bn0)(x)
-
-        if activation == "leakyrelu":
-            x = klayers.LeakyReLU(alpha=0.03, name=name_act0)(x)
-        else:
-            x = klayers.Activation(activation, name=name_act0)(x)
-
-        x = klayers.Conv2D(*args,
-                           name=name_conv1,
-                           padding="same",
+                           use_bias=False,
                            **kwds)(x)
+        return x
 
-        x = klayers.BatchNormalization(axis=get_bn_axis(),
-                                       name=name_bn1)(x)
+    def bn_(x, name):
+        return bn(x, prefix + name)
 
-        x = klayers.add([tensor, x], name=name_add)
-        if activation == "leakyrelu":
-            x = klayers.LeakyReLU(alpha=0.03, name=name_act_after)(x)
+    def act_(x, name):
+        return act(x, activation, prefix + name)
+
+    def block(tensor):
+        if simple:
+            x = conv(tensor, "conv")
+            x = bn_(x, "bn")
+
+            if dropout:
+                x = klayers.Dropout(dropout)(x)
+
         else:
-            x = klayers.Activation(activation, name=name_act_after)(x)
+            x = conv(tensor, "conv0")
+            x = bn_(x, "bn0")
+            x = act_(x, "act0")
+
+            x = conv(x, "conv1", kernel_size=1)
+            x = bn_(x, "bn1")
+            x = act_(x, "act1")
+
+            if dropout:
+                x = klayers.Dropout(dropout)(x)
+
+            x = conv(x, "conv2")
+            x = bn_(x, "bn2")
+
+        x = klayers.add([tensor, x], name=prefix + "add")
+        return act_(x, "act_final")
+
+    return block
+
+
+def residual_block_v2(filter_size, prev_filter_size, kernel_size, num_convs,
+                      dropout=-1, activation="relu", prefix="residual_block_",
+                      **kwds):
+
+    # kwds -> passed through to Conv2D
+    for n in "padding use_bias name":
+        assert n not in kwds
+
+    def conv(x, step):
+        name = prefix + "conv%s" % step
+        x = klayers.Conv2D(filter_size, kernel_size,
+                           name=name,
+                           padding="same",
+                           use_bias=False,
+                           **kwds)(x)
+        return x
+
+    def bn_(x, step):
+        return bn(x, prefix + "bn_%s" % step)
+
+    def act_(x, step):
+        return act(x, activation, prefix + "act_%s" % step)
+
+    def block(tensor):
+        x = tensor
+        for i in range(num_convs - 1):
+            x = bn_(x, i + 1)
+            x = act_(x, i + 1)
+            x = conv(x, i + 1)
+
+        x = bn_(x, num_convs)
+        x = act_(x, num_convs)
+
+        if dropout > 0:
+            x = klayers.Dropout(dropout)(x)
+
+        x = conv(x, num_convs)
+
+        if filter_size != prev_filter_size:
+            if is_channels_first():
+                tensor = klayers.Conv2D(filter_size, (1, 1), activation='linear', padding='same')(tensor)
+            else:
+                tensor = klayers.Conv2D(filter_size, (1, 1), activation='linear', padding='same')(tensor)
+
+        x = klayers.add([tensor, x], name=prefix + "add")
 
         return x
 
@@ -95,12 +148,7 @@ def ResidualBlock(*args, **kwds):
 def get_network_model(conf):
     assert isinstance(conf, confs.NNModelConfig)
 
-    # fancy l2 regularizer stuff
-    extra_params = {}
-    if conf.l2_regularisation:
-        extra_params["kernel_regularizer"] = keras_regularizers.l2(1e-4)
-
-    activation = 'leakyrelu' if conf.leaky_relu else 'relu'
+    activation = 'leakyrelu' if conf.leaky_relu else 'elu'
 
     # inputs:
     if is_channels_first():
@@ -114,33 +162,61 @@ def get_network_model(conf):
                                             conf.input_channels),
                                      name="inputs_board")
 
-    # initial conv2d/Resnet on cords
-    layer = Conv2DBlock(conf.cnn_filter_size, conf.cnn_kernel_size,
-                        padding='same',
-                        activation=activation,
-                        name='initial', **extra_params)(inputs_board)
+    v2 = False
+    if v2:
+        layer = klayers.Conv2D(conf.cnn_filter_size, 1,
+                               padding="same",
+                               use_bias=False,
+                               name='initial-conv')(inputs_board)
 
-    for i in range(conf.residual_layers):
-        layer = ResidualBlock(conf.cnn_filter_size,
-                              conf.cnn_kernel_size,
-                              name="ResLayer_%s" % i,
-                              activation=activation,
-                              **extra_params)(layer)
+        filter_size = prev_filter_size = conf.cnn_filter_size
+        incr_size = 0
+        for i, c in enumerate([1, 1, 1, 1, 1, 2, 2, 2, 3, 3, 2, 2, 3, 1, 1, 1, 1, 1]):
+            if i > 0 and i % 3 == 0:
+                filter_size += incr_size
 
-    number_of_policies = 1
-    if conf.multiple_policies:
-        number_of_policies = conf.role_count
+            layer = residual_block_v2(filter_size, prev_filter_size,
+                                      conf.cnn_kernel_size,
+                                      c,
+                                      prefix="ResLayer_%s_" % i,
+                                      dropout=0.35,
+                                      activation=activation)(layer)
+            prev_filter_size = filter_size
+    else:
+        # AG0 way:
 
+        # initial conv2d/Resnet on cords
+        layer = conv2d_block(conf.cnn_filter_size, conf.cnn_kernel_size,
+                             activation=activation,
+                             name='initial')(inputs_board)
+
+        for i in range(conf.residual_layers):
+            layer = residual_block_v1(conf.cnn_filter_size,
+                                      conf.cnn_kernel_size,
+                                      prefix="ResLayer_%s_" % i,
+                                      activation=activation)(layer)
+
+        # would make sense to add these, but it seems to hurt more than anything.  Straight line
+        # all the way eh?
+        # XXX
+        # layer = klayers.BatchNormalization(axis=get_bn_axis(),
+        #                                    name="final_bn")(layer)
+        # layer = act(layer, activation, "final_act")
+
+    # policy
+    ########
+    # similar to AG0, but with multiple policy heads
+    assert conf.multiple_policies
+    number_of_policies = conf.role_count
     assert number_of_policies == len(conf.policy_dist_count)
 
     policy_heads = []
     for idx, count in enumerate(conf.policy_dist_count):
         # residual net -> flattened for policy head
-        to_flatten = Conv2DBlock(2, 1,
-                                 name='to_flatten_policy_head_%s' % idx,
-                                 padding='valid',
-                                 activation=activation,
-                                 **extra_params)(layer)
+        to_flatten = conv2d_block(2, 1,
+                                  name='to_flatten_policy_head_%s' % idx,
+                                  padding='valid',
+                                  activation=activation)(layer)
 
         flat = klayers.Flatten()(to_flatten)
 
@@ -148,32 +224,69 @@ def get_network_model(conf):
         if conf.dropout_rate_policy > 0:
             flat = klayers.Dropout(conf.dropout_rate_policy)(flat)
 
-        head = klayers.Dense(count,
-                             activation="softmax",
-                             name="policy_%d" % idx,
-                             **extra_params)(flat)
+        head = klayers.Dense(count, name="policy_%d" % idx,
+                             activation="softmax")(flat)
 
         policy_heads.append(head)
 
-    # residual net -> flattened for value head
-    to_flatten = Conv2DBlock(1, 1,
-                             name='to_flatten_value_head',
-                             padding='valid',
-                             activation=activation,
-                             **extra_params)(layer)
+    # value
+    #######
+    value_version_v2 = False
+    if value_version_v2:
+        assert conf.input_columns == conf.input_rows
+        output_layer = layer
+        dims = conf.input_columns
+        while dims > 5:
+            if dims % 2 == 1:
+                output_layer = klayers.AveragePooling2D(4, 1)(output_layer)
+                dims -= 3
+            else:
+                output_layer = klayers.AveragePooling2D(2, 2)(output_layer)
+                dims /= 2
 
-    flat = klayers.Flatten()(to_flatten)
+        to_flatten = klayers.Conv2D(16, 1,
+                                     name='to_flatten_value_head',
+                                     padding='valid',
+                                     activation=activation)(output_layer)
 
-    # output: value head
-    hidden = klayers.Dense(conf.value_hidden_size, activation="relu",
-                           name="value_hidden_layer", **extra_params)(flat)
+        if conf.dropout_rate_value > 0:
+            to_flatten = klayers.Dropout(conf.dropout_rate_value)(to_flatten)
 
-    if conf.dropout_rate_value > 0:
-        hidden = klayers.Dropout(conf.dropout_rate_value)(hidden)
+        flat = klayers.Flatten()(to_flatten)
 
-    value_head = klayers.Dense(conf.role_count,
-                               activation="sigmoid", name="value", **extra_params)(hidden)
+        value_head = klayers.Dense(conf.role_count,
+                                   activation="sigmoid", name="value")(flat)
+
+    else:
+        # old way, as per AG0
+        to_flatten = conv2d_block(1, 1,
+                                  name='to_flatten_value_head',
+                                  padding='valid',
+                                  activation=activation)(layer)
+        flat = klayers.Flatten()(to_flatten)
+        hidden = klayers.Dense(64, name="value_hidden_layer",
+                               activation="relu")(flat)
+
+        if conf.dropout_rate_value > 0:
+            hidden = klayers.Dropout(conf.dropout_rate_value)(hidden)
+
+        value_head = klayers.Dense(conf.role_count,
+                                   activation="sigmoid", name="value")(hidden)
 
     # model:
     outputs = policy_heads + [value_head]
-    return keras_models.Model(inputs=[inputs_board], outputs=outputs)
+
+    model = keras_models.Model(inputs=[inputs_board], outputs=outputs)
+
+    # add in weight decay?  XXX rename conf to reflect it is weight decay and use +ve value instead of hard coded value.
+    if conf.l2_regularisation:
+        for layer in model.layers:
+            # XXX To get global weight decay in keras regularizers have to be added to every layer
+            # in the model. In my models these layers are batch normalization (beta/gamma
+            # regularizer) and dense/convolutions (W_regularizer/b_regularizer) layers.
+
+            if hasattr(layer, 'kernel_regularizer'):
+                # XXX too much?  Is it doubled from paper?  XXX 5e-3 ?
+                layer.kernel_regularizer = keras_regularizers.l2(1e-4)
+
+    return model
